7eaca2fe8a2c58f226aa7023b649ab67617a5d3b
Dhyata Garg <dhyatagarg@gmail.com>
2021-03-05 14:38:43 +0530

    Add functioning to forward and backward buttons

--- src/Components/Frame.js
+++ src/Components/Frame.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Screen from './Screen';
-import Wheel from './Wheel';
 
 function Frame() {
+
+    const mainMenuItems = ["Music", "Videos", "Photos", "Extras", "Now Playing", "Games", "Settings"];
+    const [currentMenuItem, setCurrentMenuItem] = useState(0);
+
+    const onClickForwardButton = () => {
+        if (currentMenuItem === mainMenuItems.length - 1) {
+            setCurrentMenuItem(0);
+        } else {
+            setCurrentMenuItem(currentMenuItem + 1);
+        }
+
+        console.log(currentMenuItem);
+
+        return currentMenuItem;
+    }
+
+    const onClickBackwardButton = () => {
+        if (currentMenuItem === 0) {
+            setCurrentMenuItem(mainMenuItems.length - 1);
+        } else {
+            setCurrentMenuItem(currentMenuItem - 1);
+        }
+
+        console.log(currentMenuItem);
+
+        return currentMenuItem;
+    }
+
+
     return (
+
         <div className="frame">
-            <Screen></Screen>
-            <Wheel></Wheel>
+            <Screen currentMenuItem = {currentMenuItem}></Screen>
+
+            <div id="outer-wheel">
+                <p id="menu-btn">MENU</p>
+                <i className="fas fa-fast-forward" id="next-btn" onClick={onClickForwardButton}></i>
+                <i className="fas fa-fast-backward" id="previous-btn" onClick={onClickBackwardButton}></i>
+                <i className="fas fa-play" id="play-btn"></i>
+                <i className="fas fa-pause" id="pause-btn"></i>
+                <div id="center-button"></div>
+        </div>
         </div>
     )
 }
--- src/Components/Screen.js
+++ src/Components/Screen.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import PurpleFlowers from '../Wallpapers/flowers/PurpleFlowers.jpeg'
 
-function Screen() {
-    const menuItems = ["Videos", "Photos", "Extras", "Now Playing", "Games"];
+function Screen(props) {
+    const menuItems = ["Music","Videos", "Photos", "Extras", "Now Playing", "Games", "Settings"];
+    console.log(menuItems[props.currentMenuItem] + props.currentMenuItem)
     return (
         <React.Fragment>
-        <div id="screen">
-            <img src={PurpleFlowers} alt="Wallpaper" id="wallpaper-image" />
-            <div id="menu-bar">
-                <div id="upper-nav">
-                    <p className="menu-items" id="nav-heading"><b>iPod</b></p>
-                    <i className="fas fa-battery-full"></i>
-                </div>
-                <hr width="100%" />
-                <p className="menu-items" id="selected">Music</p>
-                {/* <p className="menu-items">Videos</p>
-                    <p className="menu-items">Photos</p>
-                    <p className="menu-items">Extras</p>
-                    <p className="menu-items">Now Playing</p>
-                    <p className="menu-items">Games</p>
-                    <p className="menu-items">Settings</p> */}
-                {menuItems.map((items) => (<p className="menu-items">{items}</p>))}
-            </div>
+            <div id="screen">
+                <img src={PurpleFlowers} alt="Wallpaper" id="wallpaper-image" />
+                <div id="menu-bar">
+                    <div id="upper-nav">
+                        <p className="menu-items" id="nav-heading"><b>iPod</b></p>
+                        <i className="fas fa-battery-full"></i>
+                    </div>
+                    <hr width="100%" />
+                    {menuItems.map((items) => (items === menuItems[props.currentMenuItem] ? <p className="menu-items" id="selected" key={items}>{items}</p> : <p className="menu-items" key={items}>{items}</p>))}
+</div>
 
-            
-        <div id="screen-glass"></div>
-        </div>
+                    <div id="screen-glass"></div>
+                </div>
+     
         </React.Fragment>
     )
 }
